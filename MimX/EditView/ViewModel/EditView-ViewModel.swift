a9340cc1b4037089f0aef83b242915ce7a9f11e3
FG <69600609+guryelf@users.noreply.github.com>
2023-11-28 15:59:56 +0300

    Update EditView-ViewModel.swift

--- MimX/EditView/ViewModel/EditView-ViewModel.swift
+++ MimX/EditView/ViewModel/EditView-ViewModel.swift
@@ -9,29 +9,12 @@ import Foundation
 import AVKit
 
 class EditViewViewModel : ObservableObject{
-    @Published var images = [UIImage]()
-    @Published var asset : AVAsset?
     
-    func returnAsset(video:Video) {
-        let asset = AVAsset(url: URL(string: video.videoURL)!)
-        self.asset = asset
-    }
-    
-    
-    func generateImage(url: URL, compressionQuality: Double = 0.05) {
-        let duration = CMTimeGetSeconds(self.asset?.duration ?? CMTime(value: 0, timescale: 0))
-        let step = duration/8
-        let imgGenerator = AVAssetImageGenerator(asset: self.asset!)
-        for second in stride(from: 0, to: duration, by: step){
-            print(second)
-            do{
-                let cgImage = try imgGenerator.copyCGImage(at: .init(seconds: Double(second), preferredTimescale: 1), actualTime: nil)
-                let uiImage = UIImage(cgImage: cgImage)
-                self.images.append(uiImage)
-            }catch{
-                print(error.localizedDescription)
-            }
+    func generateSliderView(url:URL) -> [URL]?{
+        var imageURLS = [Data]()
+        ImageGenerator().generateImages(url: url) { images in
+            imageURLS = images
         }
-        print("images:",self.images.count)
+        return ImageGenerator().saveImages(url: url, images: imageURLS)
     }
 }
